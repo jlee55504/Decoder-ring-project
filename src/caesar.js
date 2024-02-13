b974daf1fa1e983ced291d0d643dc8a55fe1fce6
Jason <jlee55504@gmail.com>
2024-02-13 12:03:25 -0800

    Updated caesar.js.  Will negative shift numbers now.

--- src/caesar.js
+++ src/caesar.js
@@ -5,10 +5,46 @@
 
 const caesarModule = (function () {
   // you can add any code you want within this function scope
-
+  let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   function caesar(input, shift, encode = true) {
     // your solution code here
+    let answer = "";
+    let currLetter;
+    let currIndex;
+    let newIndex;
+    let code = input.toLowerCase();
     if (shift === 0 || shift < -25 || shift > 25) return false;
+    console.log(input)
+    console.log(shift);
+ 
+    for (let i = 0; i < code.length; i++) {
+      if (!alphabet.includes(code[i])) {
+        answer += code[i];
+        continue;
+      }
+      if (shift < 0) {
+        currIndex = alphabet.indexOf(code[i]);
+        if (currIndex + shift < 0) {
+          console.log("minus number > 25");
+           newIndex = currIndex + shift + 25;
+          answer += alphabet[newIndex + 1];
+          continue;
+        }
+        console.log("minus number < 25")
+        answer += alphabet[currIndex + shift];
+      } else if (shift > 0) {
+        currIndex = alphabet.indexOf(code[i]);
+        if (currIndex + shift > 25) {
+          console.log("Positive number > 25")
+          newIndex = currIndex + shift - 25;
+          answer += alphabet[newIndex -1];
+          continue;
+        }
+        console.log("Positive number < 25")
+        answer += alphabet[currIndex + shift];
+      }
+    }
+    return answer;
   }
 
   return {
