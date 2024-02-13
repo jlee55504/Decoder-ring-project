fa62812c2cad2142c3204760c55dafa68f371519
Jason <jlee55504@gmail.com>
2024-02-13 13:55:30 -0800

    updated caesar.test.js file to check if shift is equal to 0, is less than -25, greater than 25 or shift value isn't present

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,1 +1,26 @@
 // Write your tests here!
+const { expect } = require("chai");
+const { caesar } = require("../src/caesar");
+
+describe("caesar", () => {
+    it("should return false if shift value isn't present", () => {
+      const expected = false
+      const actual = caesar("thinkful")
+      expect(actual).to.be.false;
+    });
+     it("should return false if shift value is equal to 0", () => {
+      const expected = false
+      const actual = caesar("thinkful", 0)
+      expect(actual).to.be.false;
+    });
+    it("should return false if shift value is less than -25", () => {
+      const expected = false
+      const actual = caesar("thinkful", -25)
+      expect(actual).to.be.false;
+    });
+    it("should return false if shift value is greater than 25", () => {
+      const expected = false
+      const actual = caesar("thinkful", 26)
+      expect(actual).to.be.false;
+    });
+  });
