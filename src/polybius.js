76f5c68b25c3b881476bafd3406d15348d5675d2
Jason <jlee55504@gmail.com>
2024-02-13 17:41:51 -0800

    Setup initial code for polybius.js

--- src/polybius.js
+++ src/polybius.js
@@ -8,6 +8,14 @@ const polybiusModule = (function () {
 
   function polybius(input, encode = true) {
     // your solution code here
+    let answer = "";
+    let alphabet = [ {"11": "a" }, { "21": "b" }, { "31": "c" }, { "41": "d" }, { "51": "e" }, { "12": "f" }, { "22": "g" }, { "32": "h" }, { "42": "i/j"}, { "52": "k" }, { "13": "l" }, { "23": "m" }, { "33": "n" }, { "43": "o" }, { "53": "p" }, { "14": "q" }, { "24": "r" }, { "34": "s" }, { "44": "t" }, { "54": "u" }, { "15": "v" }, { "25": "w" }, { "35": "x" }, { "45": "y" }, { "55": "z" },];
+    
+    const encodeCode = input => {
+      
+    }
+    if (encode === true) encodeCode(input);
+    return answer;
   }
 
   return {
