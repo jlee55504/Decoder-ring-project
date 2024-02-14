eefc3deed6eb036c9870e13794921761436a336b
Jason <jlee55504@gmail.com>
2024-02-13 20:09:08 -0800

    updated polybius.js to encrypt messages (input) properly.  Decoding messages (input) is still unfinished

--- src/polybius.js
+++ src/polybius.js
@@ -9,15 +9,44 @@ const polybiusModule = (function () {
   function polybius(input, encode = true) {
     // your solution code here
     let answer = "";
-    let alphabet = [ {"11": "a" }, { "21": "b" }, { "31": "c" }, { "41": "d" }, { "51": "e" }, { "12": "f" }, { "22": "g" }, { "32": "h" }, { "42": "i/j"}, { "52": "k" }, { "13": "l" }, { "23": "m" }, { "33": "n" }, { "43": "o" }, { "53": "p" }, { "14": "q" }, { "24": "r" }, { "34": "s" }, { "44": "t" }, { "54": "u" }, { "15": "v" }, { "25": "w" }, { "35": "x" }, { "45": "y" }, { "55": "z" },];
+    /*Alaphabet with each letter in its own object with the letter as the value
+     and the matching number as the key.*/
+    let alphabet = [ { "11": "a" }, { "21": "b" }, { "31": "c" }, { "41": "d" }, { "51": "e" }, { "12": "f" }, { "22": "g" }, { "32": "h" }, { "42": "(i/j)"}, { "52": "k" }, { "13": "l" }, { "23": "m" }, { "33": "n" }, { "43": "o" }, { "53": "p" }, { "14": "q" }, { "24": "r" }, { "34": "s" }, { "44": "t" }, { "54": "u" }, { "15": "v" }, { "25": "w" }, { "35": "x" }, { "45": "y" }, { "55": "z" },];
     
+    /*Makes sure message (input) a string and all lowercase*/
+    input = input.toString().toLowerCase();
+
+
+   // console.log(input);
+
+   /*function used to encode a message (input) if encode parameter is set to true*/
     const encodeCode = input => {
-      
+      for (let i = 0; i < input.length; i++) {
+        for (let j = 0; j < alphabet.length;  j++) {
+       //console.log(Object.values(alphabet[j]).toString())
+        /*Checks if message (input) contains "i" or "j". If so, "42" is 
+        added to the answer*/
+          if (input[i] === "i" || input[i] === "j") {
+            answer += "42";
+            break;
+      }   /*Checks if message (input) contains spaces. If so, a space is 
+      added to the answer*/
+          else if (input[i] === " ") {
+          answer += input[i];
+          break;
+        /*Matches letters in the message (input) with the corresponding number 
+        ("key") and adds it to the answer*/
+      }   else if (input[i] === Object.values(alphabet[j]).toString()) {
+            answer += Object.keys(alphabet[j]);
+            break;
+      }
+      }
     }
+  }
     if (encode === true) encodeCode(input);
     return answer;
   }
-
+//console.log(polybius("Hello world"));
   return {
     polybius,
   };
