7c98eb4581f577779c0ab4400a5d97ec9706b658
Jason <jlee55504@gmail.com>
2024-02-13 16:28:50 -0800

    Updated caesar.test.js to pass all requirements given by Chegg Skils

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -4,23 +4,91 @@ const { caesar } = require("../src/caesar");
 
 describe("caesar", () => {
     it("should return false if shift value isn't present", () => {
-      const expected = false
-      const actual = caesar("thinkful")
+      const expected = false;
+      const actual = caesar("thinkful");
       expect(actual).to.be.false;
     });
      it("should return false if shift value is equal to 0", () => {
-      const expected = false
-      const actual = caesar("thinkful", 0)
+      const expected = false;
+      const actual = caesar("thinkful", 0);
       expect(actual).to.be.false;
     });
     it("should return false if shift value is less than -25", () => {
-      const expected = false
-      const actual = caesar("thinkful", -25)
+      const expected = false;
+      const actual = caesar("thinkful", -25);
       expect(actual).to.be.false;
     });
     it("should return false if shift value is greater than 25", () => {
-      const expected = false
-      const actual = caesar("thinkful", 26)
+      const expected = false;
+      const actual = caesar("thinkful", 26);
       expect(actual).to.be.false;
     });
+    it("should return an answer with spaces maintained throughout", () => {
+        const expected = "bpqa qa i amkzmb umaaiom!";
+        const actual = caesar("This is a secret message!", 8)
+        expect(actual).to.equal(expected);
+      });
+      it("should return an answer with non-letters included", () => {
+        const expected = "$bpqa qa i amkzmb umaaiom!";
+        const actual = caesar("$This is a secret message!", 8);
+        expect(actual).to.equal(expected);
+      });
+      it("should return an answer with the correctly shifted numbers if the 'shift' number is positive", () => {
+        const expected = "wklqnixo";
+        const actual = caesar("thinkful", 3);
+        expect(actual).to.equal(expected);
+      });
+      it("should return an answer with the correctly shifted numbers if the 'shift' number is negative", () => {
+        const expected = "qefkhcri";
+        const actual = caesar("thinkful", -3);
+        expect(actual).to.equal(expected);
+      });
+      it("should return an answer with the correctly shifted numbers if the 'shift' number is longer than the alphabet", () => {
+        const expected = "ccc";
+        const actual = caesar("zzz", 3);
+        expect(actual).to.equal(expected);
+      });
+      it("should return an answer with the correctly shifted numbers while ignoring capital letters", () => {
+        const expected = "wklqnixo";
+        const actual = caesar("Thinkful", 3);
+        expect(actual).to.equal(expected);
+      });
+      it("should return an answer with the correctly shifted numbers if 'encode' is set to 'false' (decrypt a message (input))", () => {
+        const expected = "thinkful";
+        const actual = caesar("wklqnixo", 3, false);
+        expect(actual).to.equal(expected);
+      });
+
+
+
+      it("should return an answer with spaces maintained throughout and 'encode' is set to false", () => {
+        const expected = "thin kful ";
+        const actual = caesar("wklq nixo ", 3, false);
+        expect(actual).to.equal(expected);
+      });
+      it("should return an answer with non-letters included and 'encode' is set to false", () => {
+        const expected = "$thinkful!";
+        const actual = caesar("$wklqnixo!", 3, false);
+        expect(actual).to.equal(expected);
+      });
+      it("should return an answer with the correctly shifted numbers if the 'shift' number is positive and 'encode' is set to false", () => {
+        const expected = "thinkful";
+        const actual = caesar("wklqnixo", 3, false);
+        expect(actual).to.equal(expected);
+      });
+      it("should return an answer with the correctly shifted numbers if the 'shift' number is negative and 'encode' is set to false", () => {
+        const expected = "ddd";
+        const actual = caesar("aaa", -3, false);
+        expect(actual).to.equal(expected);
+      });
+      it("should return an answer with the correctly shifted numbers if the 'shift' number is longer than the alphabet and 'encode' is set to false", () => {
+        const expected = "xxx";
+        const actual = caesar("aaa", 3, false);
+        expect(actual).to.equal(expected);
+      });
+      it("should return an answer with the correctly shifted numbers while ignoring capital letters and 'encode' is set to false", () => {
+        const expected = "thinkful";
+        const actual = caesar("Wklqnixo", 3, false);
+        expect(actual).to.equal(expected);
+      });
   });
