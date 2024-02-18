e4473b27af8bd42d517cb84889931267fda9203a
Jason <jlee55504@gmail.com>
2024-02-17 19:33:10 -0800

    Started code for substitution.js. Will return false if substitution alphabet (alphabet) doesn't exist or isn't 26 characters long. Turns all substitution alphabet (alphabet) characters to lowercase and into a string. Returns false if any characters in the substitution alphabet (alphabet) repeat.  Right now, will not encrypt or decrypt a message (input).

--- src/substitution.js
+++ src/substitution.js
@@ -8,8 +8,36 @@ const substitutionModule = (function () {
 
   function substitution(input, alphabet, encode = true) {
     // your solution code here
-  }
+    let answer = "";
+    /*Checks if the substitution alphabet (alphabet) length is exactly 26 or exists. If not, it returns false*/
+    if (!alphabet || alphabet.length !== 26) return false;
+    let alphabetObj = {};
+    /*Makes sure the substitution alphabet (alphabet) is a string and all lowercase*/
+    alphabet = alphabet.toString().toLowerCase();
+    /*Checks that all characters in the substitution alphabet (alphabet) are unique and don't reappear. 
+    If so, it returns false*/
+    for (let i = 0; alphabet.length; i++) {
+      const character = alphabet[i];
+      const index = alphabet.indexOf(character)
+      if (alphabet.includes(character, index+1)) {
+        return false;
+      } 
+    }
+    
+    const encrypt = () => {
 
+    }
+
+    const decrypt = () => {
+
+    }
+    /*Encodes message (input) if "encode" is set to true*/
+    if (encode === true) encrypt();
+    /*Decodes message (input) if "encode" is set to false*/
+    else if (encode === false) decrypt();
+    return answer;
+  }
+console.log(substitution("thinkful", "$wae&zrdxtfcygvuhbijnokm"))
   return {
     substitution,
   };
