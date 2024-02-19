f830af037ce2cc9dff3a6218a1ac9b816acbb033
Jason <jlee55504@gmail.com>
2024-02-18 21:01:06 -0800

    Updated substitution.js by making all messages (input) all lowercase and a string. Substitution.js will now also decode messages (input) by matching the message (input) with the alphabet values in alphabetArr and add the correct keys to the answer.  Passes all Chegg Skills tests. Still need to write all tests for substitution.test.js and explain all functions in the README.md file.

--- src/substitution.js
+++ src/substitution.js
@@ -11,10 +11,10 @@ const substitutionModule = (function () {
     let answer = "";
     /*Checks if the substitution alphabet (alphabet) length is exactly 26 or exists. If not, it returns false*/
     if (!alphabet || alphabet.length !== 26) return false;
-    let alphabetArr = [{ "a": "" } ,{ "b": "" }, { "c": "" } , { "d": "" } , { "e": "" } , { f: "" } ,
+    let alphabetArr = [ { "a": "" } , { "b": "" } , { "c": "" } , { "d": "" } , { "e": "" } , { f: "" } ,
      { g: "" } , { h: "" } , { i: "" } , { j: "" } , { k: "" } , { l: "" } , { m: "" } , { n: "" } , { o: "" } ,
       { p: "" } , { q: "" } , { r: "" } , { s: "" } , { t: "" } , { u: "" } , { v: "" } , { w: "" } ,
-       { x: "" } , { y: "" } , { z: "" } ,];
+       { x: "" } , { y: "" } , { z: "" } , ];
     /*Makes sure the substitution alphabet (alphabet) is a string and all lowercase*/
     alphabet = alphabet.toString().toLowerCase();
     /*Makes sure the message (input) is a string and all lowercase**/
@@ -30,12 +30,14 @@ const substitutionModule = (function () {
       break;
     }
 
+  /*Sets the alphabet in alphabetObj to equal the substitution alphabet (alphabet)*/
+  for (let i = 0; i < alphabetArr.length; i++) {
+    const key = Object.keys(alphabetArr[i]).toString();
+    alphabetArr[i][key] = alphabet[i];      
+  }
+
     const encrypt = (input, alphabet) => {
-    /*Sets the alphabet in alphabetObj to equal the substitution alphabet (alphabet)*/
-    for (let i = 0; i < alphabetArr.length; i++) {
-      const key = Object.keys(alphabetArr[i]).toString();
-      alphabetArr[i][key] = alphabet[i];      
-    }
+  
    
     /*Matches the message characters (input) with the substitution alphabet (alphabet) in alphabetObj 
     and adds the substitution alphabet characters to the answer*/
@@ -61,15 +63,35 @@ const substitutionModule = (function () {
     }
 
     const decrypt = (input, alphabet) => {
-
+      /*Matches the message characters (input) with the substitution alphabet (alphabet) in alphabetObj 
+      and adds the substitution alphabet characters to the answer*/
+      for (let i = 0; i < input.length; i++) {
+        /*Current input character*/
+        const inputChar = input[i];
+        /*Adds a space to the answer if there's one in the current input character*/
+        if (inputChar === " ") {
+          answer += inputChar;
+          continue;
+      }
+        for (const letters of alphabetArr) {
+          /*current 'letter' value in alphabetObj*/
+          const letter = Object.values(letters).toString();
+          /*Matches the current input character (input) with the substitution alphabet value in alphabetObj 
+          and adds the matching key to the answer*/
+          if(inputChar === letter) {
+            answer += Object.keys(letters).toString();
+            break;
+        }
+      }
+    }
     }
     /*Encodes message (input) if "encode" is set to true*/
     if (encode === true) encrypt(input, alphabet);
     /*Decodes message (input) if "encode" is set to false*/
-    else if (encode === false) decrypt();
+    else if (encode === false) decrypt(input, alphabet);
     return answer;
   }
-console.log(substitution("thinkful", "xoyqmcgrukswaflnthdjpzibev"))
+console.log(substitution("jrufscpw", "xoyqmcgrukswaflnthdjpzibev", false))
   return {
     substitution,
   };
