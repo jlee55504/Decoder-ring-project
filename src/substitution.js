08e7eae27f9ac7dcc4c7659763397b844dd60a14
Jason <jlee55504@gmail.com>
2024-02-19 16:34:05 -0800

    Cleaned up syntax with spaces (tab button).

--- src/substitution.js
+++ src/substitution.js
@@ -11,6 +11,7 @@ const substitutionModule = (function () {
     let answer = "";
     /*Checks if the substitution alphabet (alphabet) length is exactly 26 or exists. If not, it returns false*/
     if (!alphabet || alphabet.length !== 26) return false;
+    /*Array holding 'letter' objects with letters as keys (in alphabetical order) and empty values*/
     let alphabetArr = [ { "a": "" } , { "b": "" } , { "c": "" } , { "d": "" } , { "e": "" } , { f: "" } ,
      { g: "" } , { h: "" } , { i: "" } , { j: "" } , { k: "" } , { l: "" } , { m: "" } , { n: "" } , { o: "" } ,
       { p: "" } , { q: "" } , { r: "" } , { s: "" } , { t: "" } , { u: "" } , { v: "" } , { w: "" } ,
@@ -20,7 +21,7 @@ const substitutionModule = (function () {
     /*Makes sure the message (input) is a string and all lowercase**/
     input = input.toString().toLowerCase();
     /*Checks that all characters in the substitution alphabet (alphabet) are unique and don't reappear. 
-    If so, it returns false*/
+    If not, it returns false*/
     for (let i = 0; alphabet.length; i++) {
       const character = alphabet[i];
       const index = alphabet.indexOf(character);
@@ -30,39 +31,38 @@ const substitutionModule = (function () {
       break;
     }
 
-  /*Sets the alphabet in alphabetObj to equal the substitution alphabet (alphabet)*/
-  for (let i = 0; i < alphabetArr.length; i++) {
-    const key = Object.keys(alphabetArr[i]).toString();
-    alphabetArr[i][key] = alphabet[i];      
+    /*Sets the alphabet in alphabetObj to equal the substitution alphabet (alphabet)*/
+    for (let i = 0; i < alphabetArr.length; i++) {
+      const key = Object.keys(alphabetArr[i]).toString();
+      alphabetArr[i][key] = alphabet[i];      
   }
 
-    const encrypt = (input, alphabet) => {
-  
+    const encrypt = (input) => {
    
-    /*Matches the message characters (input) with the substitution alphabet (alphabet) in alphabetObj 
-    and adds the substitution alphabet characters to the answer*/
-    for (let i = 0; i < input.length; i++) {
-      /*Current input character*/
-      const inputChar = input[i];
-      /*Adds a space to the answer if there's one in the current input character*/
-      if (inputChar === " ") {
-        answer += inputChar;
-        continue;
+      /*Matches the message characters (input) with the substitution alphabet (alphabet) in alphabetObj 
+      and adds the substitution alphabet characters to the answer*/
+      for (let i = 0; i < input.length; i++) {
+        /*Current input character*/
+        const inputChar = input[i];
+        /*Adds a space to the answer if there's one in the current input character*/
+        if (inputChar === " ") {
+          answer += inputChar;
+          continue;
       }
-      for (const letters of alphabetArr) {
-        /*current 'letter' key in alphabetObj*/
-        const letter = Object.keys(letters).toString();
-        /*Matches the current input character (input) with the substitution alphabet key in alphabetObj 
-        and adds the matching value to the answer*/
-        if(inputChar === letter) {
-          answer += Object.values(letters).toString();
-          break;
+        for (const letters of alphabetArr) {
+          /*current 'letter' key in alphabetObj*/
+          const letter = Object.keys(letters).toString();
+          /*Matches the current input character (input) with the substitution alphabet key in alphabetObj 
+          and adds the matching value to the answer*/
+          if(inputChar === letter) {
+            answer += Object.values(letters).toString();
+            break;
         }
       }
     }
     }
 
-    const decrypt = (input, alphabet) => {
+    const decrypt = (input) => {
       /*Matches the message characters (input) with the substitution alphabet (alphabet) in alphabetObj 
       and adds the substitution alphabet characters to the answer*/
       for (let i = 0; i < input.length; i++) {
@@ -86,12 +86,12 @@ const substitutionModule = (function () {
     }
     }
     /*Encodes message (input) if "encode" is set to true*/
-    if (encode === true) encrypt(input, alphabet);
+    if (encode === true) encrypt(input);
     /*Decodes message (input) if "encode" is set to false*/
-    else if (encode === false) decrypt(input, alphabet);
+    else if (encode === false) decrypt(input);
     return answer;
   }
-console.log(substitution("jrufscpw", "xoyqmcgrukswaflnthdjpzibev", false))
+//console.log(substitution("thinkful", "xoyqmcgrukswaflnthdjpzibev"));
   return {
     substitution,
   };
