f6fb6629d57ccde8bcfa4ce99a674c6cc8c39664
Jason <jlee55504@gmail.com>
2024-02-19 16:30:37 -0800

    Updated substitution.test.js by adding more descriptive messages for tests.

--- test/substitution.test.js
+++ test/substitution.test.js
@@ -3,29 +3,29 @@ const { expect } = require("chai");
 const { substitution } = require("../src/substitution");
 
 describe("substitution (encode)", () => {
-    it ("returns false if substitution alphabet (alphabet) doesn't exist", () => {
+    it ("returns false if substitution alphabet (alphabet) doesn't exist and encode is set to true", () => {
         const actual = substitution("thinkful");
         expect(actual).to.be.false;
     });
-    it ("returns false if substitution alphabet (alphabet) isn't exaclty 26 characters long", () => {
+    it ("returns false if substitution alphabet (alphabet) isn't exaclty 26 characters long and encode is set to true", () => {
         const actual = substitution("thinkful", "short");
         expect(actual).to.be.false;
     });
-    it ("return the correct answer with spaces maintained throughout", () => {
+    it ("return the correct answer with spaces maintained throughout and encode is set to true", () => {
         const actual = substitution("You are an excellent spy", "xoyqmcgrukswaflnthdjpzibev");
         const expected = 'elp xhm xf mbymwwmfj dne';
         expect(actual).to.equal(expected);
     });
-    it ("returns false if all characters in the substitution alphabet (alphabet) aren't unique", () => {
+    it ("returns false if all characters in the substitution alphabet (alphabet) aren't unique and encode is set to true", () => {
         const actual = substitution("thinkful", "abcabcabcabcabcabcabcabcyz");
         expect(actual).to.be.false;
     });
-    it ("returns an answer ignoring capital letters in the message (input)", () => {
+    it ("returns an answer ignoring capital letters in the message (input) and encode is set to true", () => {
         const actual = substitution("Thinkful", "xoyqmcgrukswaflnthdjpzibev");
         const expected = 'jrufscpw';
         expect(actual).to.equal(expected);
     });
-    it ("the alphabet parameter can include special characters (not letters or spaces)", () => {
+    it ("the alphabet parameter can include special characters (not letters or spaces) and encode is set to true", () => {
         const actual = substitution("message", "$wae&zrdxtfcygvuhbijnokmpl");
         const expected = "y&ii$r&";
         expect(actual).to.equal(expected);
@@ -33,35 +33,30 @@ describe("substitution (encode)", () => {
 });
 
 describe("substitution (decode)", () => {
-    it ("returns false if substitution alphabet (alphabet) doesn't exist", () => {
+    it ("returns false if substitution alphabet (alphabet) doesn't exist and encode is set to false", () => {
         const actual = substitution("thinkful", false);
         expect(actual).to.be.false;
     });
-    it ("returns false if substitution alphabet (alphabet) isn't exaclty 26 characters long", () => {
+    it ("returns false if substitution alphabet (alphabet) isn't exaclty 26 characters long and encode is set to false", () => {
         const actual = substitution("thinkful", "short", false);
         expect(actual).to.be.false;
     });
-    it ("returns an answer ignoring capital letters in the message (input)", () => {
+    it ("returns an answer ignoring capital letters in the message (input) and encode is set to false", () => {
         const actual = substitution("Y&ii$r&", "$wae&zrdxtfcygvuhbijnokmpl", false);
         const expected = "message";
         expect(actual).to.equal(expected);
     });
-    it ("return the correct answer with spaces maintained throughout", () => {
+    it ("return the correct answer with spaces maintained throughout and encode is set to false", () => {
         const actual = substitution("y &ii$r&", "$wae&zrdxtfcygvuhbijnokmpl", false);
         const expected = "m essage";
         expect(actual).to.equal(expected);
     });
-    it ("the alphabet parameter can include special characters (not letters or spaces)", () => {
+    it ("the alphabet parameter can include special characters (not letters or spaces) and encode is set to false", () => {
         const actual = substitution("y&ii$r&", "$wae&zrdxtfcygvuhbijnokmpl", false);
         const expected = "message";
         expect(actual).to.equal(expected);
     });
-    it ("Messages (input) can include special characters (not letters or spaces)", () => {
-        const actual = substitution("y&ii$r&", "$wae&zrdxtfcygvuhbijnokmpl", false);
-        const expected = "message";
-        expect(actual).to.equal(expected)
-    });
-    it ("returns false if all characters in the substitution alphabet (alphabet) aren't unique", () => {
+    it ("returns false if all characters in the substitution alphabet (alphabet) aren't unique and encode is set to false", () => {
         const actual = substitution("abc", "abcabcabcabcabcabcabcabcyz", false);
         expect(actual).to.be.false;
     });
